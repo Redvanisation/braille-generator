8231311efb168fcd6332252f46b027772d6ede7e
Redvanisation <red@macs-MacBook-Pro-2.local>
2018-12-17 00:57:59 -0500

    Updates

---
+++ precache-manifest.97765b3f8c5402afdc86478a17451ce3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe35ccf82f6c82a0ff2a",
+    "url": "/braille-generator/static/js/runtime~main.fe35ccf8.js"
+  },
+  {
+    "revision": "cc381509975dcc381537",
+    "url": "/braille-generator/static/js/main.cc381509.chunk.js"
+  },
+  {
+    "revision": "3440b6f1a2660ebff578",
+    "url": "/braille-generator/static/js/1.3440b6f1.chunk.js"
+  },
+  {
+    "revision": "cc381509975dcc381537",
+    "url": "/braille-generator/static/css/main.05cfb9bd.chunk.css"
+  },
+  {
+    "revision": "4caf3cab90d264729131eb9c9c03c0d8",
+    "url": "/braille-generator/index.html"
+  }
+];
